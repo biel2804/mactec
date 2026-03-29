aaa6bc82f0b2b0890fd5fa896d3f699cca0fb7db
biel2804 <gabrielmacedorodrigues@hotmail.com>
2026-03-29 01:49:47 -0300

    Add global app configuration in app-config.js
    Added global app configuration with Supabase details and optional company and shipping settings.

---
+++ app-config.js
@@ -0,0 +1,19 @@
+// CONFIG GLOBAL DO APP
+window.APP_CONFIG = {
+    SUPABASE_URL: "https://dvkcxpwhbpiltqovveue.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2a2N4cHdoYnBpbHRxb3Z2ZXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MjA5NTMsImV4cCI6MjA4OTA5Njk1M30.g7c0k_iRG0sEHwo9Wy_qWn8tONNHd1b2mxhkl97tcPk",
+
+    // OPCIONAL (recomendado já deixar preparado)
+    COMPANY: {
+        NAME: "MacTec Support",
+        CITY: "São Bernardo do Campo",
+        STATE: "SP",
+        WHATSAPP: "5511999999999"
+    },
+
+    // CONFIG DE FRETE
+    SHIPPING: {
+        PACKAGING_FEE: 5.00,
+        LOCAL_DELIVERY_RADIUS_KM: 5
+    }
+};
